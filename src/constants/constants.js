f12f8cba5b8dd7cdfe93947384290c90deef1853
Orestis Pantazos <opantazos@gmail.com>
2021-11-08 17:18:31 +0200

    added new projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,20 @@
 export const projects = [
+  // {
+  //   title: 'Best Resume Builder - iOS app template',
+  //   description: "Create your resume with just a few taps & select from our PDF templates, One page only PDF resume templates made in SwiftUI",
+  //     image: '/images/1.png',
+  //     tags: ['SwiftUI', 'Swift', 'LinkedIn', 'Firebase'],
+  //   source: 'https://github.com/Apps4Devs/ResumeBuilderUI',
+  //   visit: 'https://github.com/Apps4Devs/ResumeBuilderUI',
+  //   id: 0,
+  // },
   {
-    title: 'Best Resume Builder - iOS app template',
-    description: "Create your resume with just a few taps & select from our PDF templates, One page only PDF resume templates made in SwiftUI",
-      image: '/images/1.png',
-      tags: ['SwiftUI', 'Swift', 'LinkedIn', 'Firebase'],
-    source: 'https://github.com/Apps4Devs/ResumeBuilderUI',
-    visit: 'https://github.com/Apps4Devs/ResumeBuilderUI',
+    title: 'Freelancer - Android app template',
+    description: "Freelancer Android app for Greek customers and professionals based in EU country written in Kotlin and Firebase",
+      image: '/images/2.png',
+      tags: ['Android', 'Kotlin', 'Firebase', "Laravel"],
+    source: 'https://github.com/Apps4Devs/FreelancerUI',
+    visit: 'https://github.com/Apps4Devs/FreelancerUI',
     id: 0,
   }
 ];
